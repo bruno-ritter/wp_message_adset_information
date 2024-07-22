d857d1c28fd64bdcf6294d8b85045fc451a20b30
bruno <bruno.maritter@gmail.com>
2024-07-22 00:35:51 -0300

    Resultado e custo por ressultado.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from creds import account, access_token
+from creds import FB_ACCOUNT, FB_ACCESS_TOKEN
 from datetime import date, timedelta
 
 def get_data_meta_ads(account, access_token, timeframe='last_7d'):
@@ -12,17 +12,34 @@ def get_data_meta_ads(account, access_token, timeframe='last_7d'):
 
     return json_data
 
+dict_objective_results = {
+    'LINK_CLINKS': 'link_click'
+}
+
 yesterday = (date.today() - timedelta(days=1))
 
-json_adset = get_data_meta_ads(account, access_token)
+json_adset = get_data_meta_ads(account=FB_ACCOUNT, access_token=FB_ACCESS_TOKEN)
 
 json_adset_yesterday = [item for item in json_adset['data'] if item['date_start'] == yesterday.strftime("%Y-%m-%d")]
 
-# print(json.dumps(json_adset_yesterday, indent=2))
+# print(json.dumps(json_adset, indent=2))
  
-amount_spend_in_period = round(sum(float(item['spend']) for item in json_adset['data']), 2)
-amount_spend_yesterday = round(sum(float(item['spend']) for item in json_adset_yesterday), 2)
+amount_spend_in_period = round(sum(float(item['spend']) for item in json_adset['data']), 0)
+amount_spend_yesterday = round(sum(float(item['spend']) for item in json_adset_yesterday), 0)
 
-message = f"Nos últimos 7 dias foram investidos R$ {amount_spend_in_period}. E ontem ({yesterday.strftime('%d/%m')}) foram investidos R$ {amount_spend_yesterday}."
+result_objective = 0
+for campaign in json_adset['data']:
+    try:
+        for action_type in campaign['actions']:
+            if action_type['action_type'] == 'link_click':
+                result_objective += int(action_type['value'])
+    except: KeyError
+
+cost_per_result = round(amount_spend_in_period / result_objective, 2)
+
+campaign_name = ''
+best_ad_name = ''
+
+message = f"Nos últimos 7 dias foram investidos R$ {int(amount_spend_in_period)}, resultando em {result_objective} cliques no link com custo de {cost_per_result} por resultado. E ontem ({yesterday.strftime('%d/%m')}) foram investidos R$ {int(amount_spend_yesterday)}. \nO melhor anúncio da semana está sendo o {best_ad_name} da campanha {campaign_name}."
 
 print(message)
